280d256989e0dbe76705de0ca17b62493046820b
Jhones William <jhones.william@outlook.com.br>
2015-12-04 22:03:13 -0200

    Criada a Area de Login
    Nesse update foi criado a classe fucionario, o controller de login,
    falta algumas implementações

---
+++ S2B/S2B/Models/Fucionario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2B.Models
+{
+    class Fucionario
+    {
+        private static bd_projeto_S2bEntitiesLogin db = new bd_projeto_S2bEntitiesLogin();//Connecta ao banco
+
+        public static bool Verificar(string login, string senha)
+        {
+            if (db.FUNCIONARIOs.Where(a => a.LOGIN.Contains(login) && a.SENHA.Contains(senha)).Count() == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
